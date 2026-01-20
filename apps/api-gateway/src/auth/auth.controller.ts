60b54093f6cb250c1f630f44be34cff13ad9054e
ZIADMOHAMED-WEB <z.maheerr2@gmail.com>
2026-01-20 11:55:28 +0200

    init commit from ziad

--- apps/api-gateway/src/auth/auth.controller.ts
+++ apps/api-gateway/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
+//only for testing purposes
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 }
